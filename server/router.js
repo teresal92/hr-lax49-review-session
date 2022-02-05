7d7507648f5e2880c49985c3063c43cf2e829cee
Teresa Lew <teresalew92@gmail.com>
2022-02-05 12:33:11 -0800

    Routed requests to appropriate controller functions

--- server/router.js
+++ server/router.js
@@ -6,16 +6,19 @@ const controller = require('./controller.js');
 // routes for retrieving all students and adding a student's name
 router
   .route('/students')
+  .get(controller.students.getStudents)
+  .post(controller.students.postName);
 
 
 // route for updating a student's name
 router
   .route('/students/:id')
+  .put(controller.students.updateName);
 
 
 // routes for adding a new student image
 router
   .route('/images')
-
+  .post(controller.images.postImg);
 
 module.exports = router;

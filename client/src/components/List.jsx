5bdc1ae1b83c07a3720043c0bc65c9650dd32201
Teresa Lew <teresalew92@gmail.com>
2022-02-05 12:36:43 -0800

    Implemented dynamically rendering student list entries in student list component

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,33 +1,39 @@
 import React from 'react';
 import ListElement from './ListElement.jsx';
 
-const List = () => (
+const List = ({students}) => (
   <div>
-    <span>
-      <div>Josh</div>
-      <img src='https://ca.slack-edge.com/T02DNK3PH-U01ATPUTRH8-0c6b5a486403-512'></img>
-    </span>
-    <span>
-      <div>Josh</div>
-      <img src='https://ca.slack-edge.com/T02DNK3PH-U01ATPUTRH8-0c6b5a486403-512'></img>
-    </span>
-    <span>
-      <div>Josh</div>
-      <img src='https://ca.slack-edge.com/T02DNK3PH-U01ATPUTRH8-0c6b5a486403-512'></img>
-    </span>
-    <span>
-      <div>Josh</div>
-      <img src='https://ca.slack-edge.com/T02DNK3PH-U01ATPUTRH8-0c6b5a486403-512'></img>
-    </span>
-    <span>
-      <div>Josh</div>
-      <img src='https://ca.slack-edge.com/T02DNK3PH-U01ATPUTRH8-0c6b5a486403-512'></img>
-    </span>
-    <span>
-      <div>Josh</div>
-      <img src='https://ca.slack-edge.com/T02DNK3PH-U01ATPUTRH8-0c6b5a486403-512'></img>
-    </span>
+    {students.map((student, i) => <ListElement student={student} key={`student-entry-${i}`}/>)}
   </div>
 );
+
+// const List = () => (
+//   <div>
+//     <span>
+//       <div>Josh</div>
+//       <img src='https://ca.slack-edge.com/T02DNK3PH-U01ATPUTRH8-0c6b5a486403-512'></img>
+//     </span>
+//     <span>
+//       <div>Josh</div>
+//       <img src='https://ca.slack-edge.com/T02DNK3PH-U01ATPUTRH8-0c6b5a486403-512'></img>
+//     </span>
+//     <span>
+//       <div>Josh</div>
+//       <img src='https://ca.slack-edge.com/T02DNK3PH-U01ATPUTRH8-0c6b5a486403-512'></img>
+//     </span>
+//     <span>
+//       <div>Josh</div>
+//       <img src='https://ca.slack-edge.com/T02DNK3PH-U01ATPUTRH8-0c6b5a486403-512'></img>
+//     </span>
+//     <span>
+//       <div>Josh</div>
+//       <img src='https://ca.slack-edge.com/T02DNK3PH-U01ATPUTRH8-0c6b5a486403-512'></img>
+//     </span>
+//     <span>
+//       <div>Josh</div>
+//       <img src='https://ca.slack-edge.com/T02DNK3PH-U01ATPUTRH8-0c6b5a486403-512'></img>
+//     </span>
+//   </div>
+// );
 
 export default List;
--- client/src/components/ListElement.jsx
+++ client/src/components/ListElement.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const ListElement = () => (
+const ListElement = ({student}) => (
   <span>
-    <div>Julian</div>
-    <img src='https://ca.slack-edge.com/T02DNK3PH-UD0AF2EBH-73605fa7261b-512'></img>
+    <div>{student.name}</div>
+    <img src={student.imgurl}></img>
   </span>
 );
 

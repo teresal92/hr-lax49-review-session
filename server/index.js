d1d9ec25808aa94e56dd938a917c018aa059afab
Teresa Lew <teresalew92@gmail.com>
2022-02-05 12:33:36 -0800

    connected router.js file

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
 server.use(express.static(path.join(__dirname, '../client/dist')));
+server.use('/class', router);
 
 
 server.listen(port, () => console.log(`listening on ${port}`))
